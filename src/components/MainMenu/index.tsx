775ae502a53081f02b4ebb317e836c14934b63e5
wwnbl <15797629@qq.com>
2022-11-16 22:13:14 +0800

    菜单数据整理

--- src/components/MainMenu/index.tsx
+++ src/components/MainMenu/index.tsx
@@ -12,31 +12,22 @@ import {useNavigate} from "react-router-dom"
 
 type MenuItem = Required<MenuProps>['items'][number];
 
-function getItem(
-    label: React.ReactNode,
-    key: React.Key,
-    icon?: React.ReactNode,
-    children?: MenuItem[],
-): MenuItem {
-    return {
-        key,
-        icon,
-        children,
-        label,
-    } as MenuItem;
-}
-
 const items: MenuItem[] = [
-    getItem('关于', '/about', <PieChartOutlined />),
-    getItem('page 1', '/page1', <DesktopOutlined />),
-    getItem('page 2', '/page2', <DesktopOutlined />),
-    getItem('User', 'sub1', <UserOutlined />, [
-        getItem('Tom', '3'),
-        getItem('Bill', '4'),
-        getItem('Alex', '5'),
-    ]),
-    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-    getItem('Files', '9', <FileOutlined />),
+    {label:'关于', key:'/about', icon:<PieChartOutlined />},
+    {label:'page 1',key: '/page1',icon: <DesktopOutlined />},
+    {label:'page 2', key:'/page2',icon:  <DesktopOutlined /> },
+    {label:'User', key:'/sub1', icon:<UserOutlined />,children:[
+        {label:'Tom', key:'3'},
+        {label:'Bill', key:'4'},
+        {label:'Alex', key:'5'}
+        ]
+    },
+    {label:'Team', key:'/sub2', icon:<TeamOutlined />,children:[
+        {label:'Team 1', key:'6'},
+        {label:'Team 2', key:'8'}
+        ]
+    },
+    {label:'Files', key:'9', icon:<FileOutlined />},
 ];
 
 const rootSubmenuKeys = ['sub1', 'sub2'];
